3460cbbbd84b29787c2a83907c8c03a2bb7d73f2
Sean Cahill <seancahill25@gmail.com>
2017-10-26 16:07:18 +0000

    first commit

---
+++ Untitled1.js
@@ -0,0 +1,21 @@
+var x= 5;
+var y= 10;
+var z= x + y;
+console.log(z);
+var name ="Sean";
+console.log("hello " + name)
+
+var numbers = [47, 22, 48, 22, 13, 85, 33, 19, 24, 36];
+
+var smallestNumber = numbers[0];
+for (var number = 1; number < numbers.length; number++) 
+{if (numbers[number] < smallestNumber) 
+     {smallestNumber = numbers[number];
+         
+     }
+    
+}
+     
+console.log(smallestNumber)
+
+
